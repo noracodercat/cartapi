8990347346b8ad03013b3bc35bcc770368ed428f
minnacat <minnacicus@gmail.com>
2019-03-05 00:34:35 +0100

    modified data models

--- models/user.js
+++ models/user.js
@@ -1,10 +1,19 @@
 var mongoose= require('mongoose');
 
+var CartSchema= new mongoose.Schema({
+    items:[
+        { 
+            id: {type:String, unique:true }, 
+            amount: Number
+        }
+    ]
+});
 
 var UserSchema= new mongoose.Schema({
     email: { type: String, unique: true },
     username: String,
-    password: String
+    password: String,
+    cart: CartSchema
 });
 
 
